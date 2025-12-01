193516e87b8ed3da6d53f0c94b0c983d0ecad879
krishguptacodes <139714105+krishguptacodes@users.noreply.github.com>
2025-12-01 09:57:14 -0500

    documentation

--- src/rewards.py
+++ src/rewards.py
@@ -8,30 +8,35 @@ Computes reward using:
 - crash / stall / success bonuses
 """
 
+#imports
 import math
 from typing import Tuple
 from pyproj import Geod
 
 geod = Geod(ellps="WGS84")
 
-
+# reward function 
 def reward(
 		plane_speed, fuel_diff, dist_traveled,
-		desired_speed=20.0,
+		desired_speed=20.0, # manuel setting target speed
 		crashed=False, stalled=False, reached=False,
 ) -> float:
 	"""Compute a simple scalar reward."""
 	speed_err = abs(plane_speed - desired_speed)
 	efficiency = dist_traveled / fuel_diff if fuel_diff > 0 else 0
 
+	#set reward 0 
 	r = 0
-	r -= 0.1 * speed_err
-	r += 0.1 * efficiency
+	r -= 0.1 * speed_err # decrease with speed error
+	r += 0.1 * efficiency #increase reward if efficient with path/fuel.
 
+	# negative rewards
 	if crashed:
 		r -= 500
 	if stalled:
 		r -= 200
+
+	#reaches goal
 	if reached:
 		r += 1000
 
